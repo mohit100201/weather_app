31dc9d45adbbe85ea80e55e81b1fabf4cb59b67f
Mohit Khandelwal <mohitkhandelwal9784@gmail.com>
2025-05-01 11:33:44 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env:{
+    API:'80bf4a5dc638430480a83031252904',
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,109 @@
+"use client";
+import { Cinzel } from "next/font/google";
 import Image from "next/image";
+import { useState, ChangeEvent, FormEvent } from "react";
+
+interface WeatherData{
+  temperature :number;
+  region :string;
+  name:string;
+  country:string;
+  unit:string;
+
+
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const[location,setLocation]=useState<string>("")
+  const[weatherData,setWeatherData]=useState<WeatherData>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const fetchData=async()=>{
+    
+    const city:string=location.trim();
+    if(city==""){
+      console.log("City not found");
+    }
+    console.log("cityName : "+city)
+    const data=await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.API} &q=${city}`)
+    console.log(data);
+    if (!data.ok) {
+      console.log("City not found");
+    }
+    else{
+      console.log("city found")
+    }
+    const response=await data.json();
+    const t:number=response.location.temp_c
+    // {console.log("temperature in celcuis : "+ response.current.temp_c )}
+ 
+    const weatherData:WeatherData={
+      temperature:response.current.temp_c,
+      region:response.location.region,
+      name:response.location.name,
+      country:response.location.country,
+      unit:"C",
+
+    }
+    setWeatherData(weatherData);
+
+  }
+
+
+  return (
+    <div>
+    <div className="flex flex-col  h-30 bg-blue-500 rounded-md mx-10 mt-2 items-center  justify-center ">
+      <h1 className="font-bold">Weather App</h1>
+
+     <div className="flex flex-row justify-between ">
+     <input type="text" placeholder="Enter Location" value={location} onChange={
+                (e: ChangeEvent<HTMLInputElement>) =>
+                  setLocation(e.target.value) 
+              }
+              />
+     <button className="bg-neutral-100  items-center rounded-md p-1  ml-3" type="button" onClick={fetchData } >Search</button>
+     </div>
+     
+     
+     
+   
+     
+
+
+      
+    </div >
+    
+<div className="flex flex-row items-center justify-center">
+
+<div className="flex flex-col items-start ">
+
+<p>CityName: </p>
+<p>State: </p>
+<p>Country: </p>
+<p>Temperature: </p>
+<p>Unit: </p>
+
+</div>
+
+<div className="flex flex-col items-start">
+
+<p>{weatherData?.name}</p>
+<p>{weatherData?.region}</p>
+<p>{weatherData?.country}</p>
+<p>{weatherData?.temperature}</p>
+<p>{weatherData?.unit}</p>
+
+
+
+</div>
+
+
+
+</div>
+ 
+
+   
+    
+</div>
+    
   );
 }
